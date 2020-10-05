0494c117081d23d1ecfe2c910a9578e4cc69d54e
Mun Kwangsu <yes08145@gmail.com>
2020-10-05 16:18:34 +0900

    commit First

---
+++ TCPSocketCl/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace TCPSocketCl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_IP1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_Port = new System.Windows.Forms.TextBox();
+            this.textBox_IP2 = new System.Windows.Forms.TextBox();
+            this.textBox_IP3 = new System.Windows.Forms.TextBox();
+            this.textBox_IP4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btn_connect = new System.Windows.Forms.Button();
+            this.btn_disconnect = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.파일ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.임시ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.끝내기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox_IP1
+            // 
+            this.textBox_IP1.Location = new System.Drawing.Point(206, 54);
+            this.textBox_IP1.Name = "textBox_IP1";
+            this.textBox_IP1.Size = new System.Drawing.Size(26, 21);
+            this.textBox_IP1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(145, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "IP      :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(145, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "PORT :";
+            // 
+            // textBox_Port
+            // 
+            this.textBox_Port.Location = new System.Drawing.Point(205, 103);
+            this.textBox_Port.Name = "textBox_Port";
+            this.textBox_Port.Size = new System.Drawing.Size(123, 21);
+            this.textBox_Port.TabIndex = 4;
+            // 
+            // textBox_IP2
+            // 
+            this.textBox_IP2.Location = new System.Drawing.Point(238, 54);
+            this.textBox_IP2.Name = "textBox_IP2";
+            this.textBox_IP2.Size = new System.Drawing.Size(26, 21);
+            this.textBox_IP2.TabIndex = 1;
+            // 
+            // textBox_IP3
+            // 
+            this.textBox_IP3.Location = new System.Drawing.Point(270, 54);
+            this.textBox_IP3.Name = "textBox_IP3";
+            this.textBox_IP3.Size = new System.Drawing.Size(26, 21);
+            this.textBox_IP3.TabIndex = 2;
+            // 
+            // textBox_IP4
+            // 
+            this.textBox_IP4.Location = new System.Drawing.Point(302, 54);
+            this.textBox_IP4.Name = "textBox_IP4";
+            this.textBox_IP4.Size = new System.Drawing.Size(26, 21);
+            this.textBox_IP4.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(232, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(9, 12);
+            this.label3.TabIndex = 10;
+            this.label3.Text = ".";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label4.Location = new System.Drawing.Point(264, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(9, 12);
+            this.label4.TabIndex = 11;
+            this.label4.Text = ".";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label5.Location = new System.Drawing.Point(296, 61);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(9, 12);
+            this.label5.TabIndex = 12;
+            this.label5.Text = ".";
+            // 
+            // btn_connect
+            // 
+            this.btn_connect.Location = new System.Drawing.Point(148, 154);
+            this.btn_connect.Name = "btn_connect";
+            this.btn_connect.Size = new System.Drawing.Size(84, 23);
+            this.btn_connect.TabIndex = 13;
+            this.btn_connect.Text = "Connect";
+            this.btn_connect.UseVisualStyleBackColor = true;
+            this.btn_connect.Click += new System.EventHandler(this.btn_connect_Click);
+            // 
+            // btn_disconnect
+            // 
+            this.btn_disconnect.Location = new System.Drawing.Point(238, 154);
+            this.btn_disconnect.Name = "btn_disconnect";
+            this.btn_disconnect.Size = new System.Drawing.Size(90, 23);
+            this.btn_disconnect.TabIndex = 14;
+            this.btn_disconnect.Text = "Disconnect";
+            this.btn_disconnect.UseVisualStyleBackColor = true;
+            this.btn_disconnect.Click += new System.EventHandler(this.btn_disconnect_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.파일ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(484, 24);
+            this.menuStrip1.TabIndex = 15;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 파일ToolStripMenuItem
+            // 
+            this.파일ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.임시ToolStripMenuItem,
+            this.끝내기ToolStripMenuItem});
+            this.파일ToolStripMenuItem.Name = "파일ToolStripMenuItem";
+            this.파일ToolStripMenuItem.ShortcutKeyDisplayString = "F4";
+            this.파일ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.파일ToolStripMenuItem.Text = "파일";
+            // 
+            // 임시ToolStripMenuItem
+            // 
+            this.임시ToolStripMenuItem.Name = "임시ToolStripMenuItem";
+            this.임시ToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.임시ToolStripMenuItem.Text = "임시";
+            // 
+            // 끝내기ToolStripMenuItem
+            // 
+            this.끝내기ToolStripMenuItem.Name = "끝내기ToolStripMenuItem";
+            this.끝내기ToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.끝내기ToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.끝내기ToolStripMenuItem.Text = "끝내기";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 189);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(460, 350);
+            this.richTextBox1.TabIndex = 16;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 561);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btn_disconnect);
+            this.Controls.Add(this.btn_connect);
+            this.Controls.Add(this.textBox_IP4);
+            this.Controls.Add(this.textBox_IP3);
+            this.Controls.Add(this.textBox_IP2);
+            this.Controls.Add(this.textBox_Port);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_IP1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_IP1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_Port;
+        private System.Windows.Forms.TextBox textBox_IP2;
+        private System.Windows.Forms.TextBox textBox_IP3;
+        private System.Windows.Forms.TextBox textBox_IP4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btn_connect;
+        private System.Windows.Forms.Button btn_disconnect;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 파일ToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ToolStripMenuItem 임시ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 끝내기ToolStripMenuItem;
+    }
+}
---
+++ TCPSocketCl/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TCPSocketCl
+{
+    public partial class Form1 : Form
+    {
+        public static bool flag = true;
+        public static Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        public static int ip1 = 0;
+        public static int ip2 = 0;
+        public static int ip3 = 0;
+        public static int ip4 = 0;
+        public static int port = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void btn_connect_Click(object sender, EventArgs e)
+        {
+            // (1) 소켓 객체 생성
+            Tboxvalue();
+            // (2) 서버 연결
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse("192.168.0.180"), 5000);
+            sock.Connect(ep);
+            string cmd = string.Empty;
+            byte[] receiverBuff = new byte[1024];
+            StringBuilder sb = new StringBuilder();
+            while (flag)
+            {
+                int n = sock.Receive(receiverBuff);
+                string strHex = BitConverter.ToString(receiverBuff);
+                richTextBox1.Text = strHex;
+                //Thread.Sleep(1000);
+            }
+            sock.Close();
+            flag = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btn_disconnect_Click(object sender, EventArgs e)
+        {
+            flag = false;
+        }
+
+        private void Tboxvalue()
+        {
+            ip1 = Convert.ToInt32(textBox_IP1.Text);
+            ip2 = Convert.ToInt32(textBox_IP2.Text);
+            ip3 = Convert.ToInt32(textBox_IP3.Text);
+            ip4 = Convert.ToInt32(textBox_IP4.Text);
+            port = Convert.ToInt32(textBox_Port.Text);
+
+        }
+    }
+}
