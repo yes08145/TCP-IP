d98e8801a5b9f9e859a8d4899a7c4ba36f906bdb
Mun Kwangsu <yes08145@gmail.com>
2020-10-12 17:41:35 +0900

    EOF 확인 작업

--- TCPSocketCl/Form1.SocketEvent.cs
+++ TCPSocketCl/Form1.SocketEvent.cs
@@ -38,27 +38,43 @@ namespace TCPSocketCl
 
         public void Connect()
         {
-            byte[] receiverBuff = new byte[16];
+            byte[] receiverBuff = new byte[1024];
             conn = true;
             try
             {
                 while (flag)
                 {
-                    sock.Send()
+                    //sock.Send()
+                    RTUP rttt = new RTUP();
                     int n = sock.Receive(receiverBuff);
                     //int resize = BitConverter.ToInt32(receiverBuff, 0);
                     strHex = BitConverter.ToString(receiverBuff);
+                    string strHexSplit = string.Empty;
+                    string log_result = string.Empty;
+                    int p_length = Convert.ToInt32(strHex.Split('-')[2], 16);
+                    //strHexSplit = strHex.Substring(0, (p_length * 2) + (p_length - 1));
+
+                    if (strHex.Split('-')[p_length - 1] == "03")
+                    {
+                        strHexSplit = strHex.Substring(0, (p_length * 2) + (p_length - 1));
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                    log_result = JudgeAction(strHexSplit);
+                    
                     if (!InvokeRequired)
                     {
-                        Log(strHex);
+                        Log(strHexSplit);
                         ListboxFocus();
                     }
                     else
                     {
-                        this.Invoke(new LogDelegate(Log), strHex);
+                        this.Invoke(new LogDelegate(Log), strHexSplit);
                         this.Invoke(new FocusDelegate(ListboxFocus));
                     }
-                    Thread.Sleep(10);
+                    //Thread.Sleep(10);
                 }
                 thread.Abort();
                 //sock.Shutdown(SocketShutdown.Both);
@@ -80,28 +96,77 @@ namespace TCPSocketCl
             }
         }
 
-        public void Send()
+        //public void Send()
+        //{
+        //    RTUP rtup = new RTUP();
+        //    rtup.usys_device_ID = 0x74;
+        //    rtup.length = 0x09;
+        //    rtup.sensor_ID = 0x01;
+        //    //0 or 1 장비 선택
+        //    rtup.ch_setting = 0x00;
+        //    // 4~20mA
+        //    rtup.data = 0x04;
+        //    //checksum
+        //    int checkSum = rtup.sof + rtup.usys_device_ID + rtup.length + rtup.sensor_ID + rtup.ch_setting + rtup.data;
+        //    rtup.check_sum = (byte)checkSum;
+        //    byte[] msg = new byte[9];
+        //    msg[0] = rtup.sof;
+        //    msg[1] = rtup.usys_device_ID;
+        //    msg[2] = rtup.length;
+        //    msg[3] = rtup.sensor_ID;
+        //    msg[4] = rtup.ch_setting;
+        //    msg[5] = rtup.data;
+        //    msg[6] = rtup.check_sum;
+        //    msg[8] = rtup.eof;
+        //}
+        //int p_length = Int32.Parse(strHex.Split('-')[2], );
+        //textsplit.Length + strHex.IndexOf(textsplit)
+
+        public string JudgeAction(string txt)
         {
             RTUP rtup = new RTUP();
-            rtup.usys_device_ID = 0x74;
-            rtup.length = 0x09;
-            rtup.sensor_ID = 0x01;
-            //0 or 1 장비 선택
-            rtup.ch_setting = 0x00;
-            // 4~20mA
-            rtup.data = 0x04;
-            //checksum
-            int checkSum = rtup.sof + rtup.usys_device_ID + rtup.length + rtup.sensor_ID + rtup.ch_setting + rtup.data;
-            rtup.check_sum = (byte)checkSum;
-            byte[] msg = new byte[9];
-            msg[0] = rtup.sof;
-            msg[1] = rtup.usys_device_ID;
-            msg[2] = rtup.length;
-            msg[3] = rtup.sensor_ID;
-            msg[4] = rtup.ch_setting;
-            msg[5] = rtup.data;
-            msg[6] = rtup.check_sum;
-            msg[7] = rtup.eof;
+            string log = string.Empty;
+            rtup.length = Convert.ToByte(txt.Split('-')[2]);
+            rtup.sensor_ID = Convert.ToByte(txt.Split('-')[3]);
+            
+            string ck_sum = string.Empty;
+            
+            ck_sum = txt.Split('-')[0] + txt.Split('-')[1] + txt.Split('-')[2] + txt.Split('-')[3] + txt.Split('-')[4];
+            if (txt.Split('-')[4] == "00")
+            {
+
+            }
+            if (rtup.sensor_ID == 01) // 전류 출력
+            {
+                ck_sum = txt.Split('-')[0] + txt.Split('-')[1] + txt.Split('-')[2] + txt.Split('-')[3] + txt.Split('-')[4];
+                rtup.check_sum = Encoding.UTF8.GetBytes(ck_sum);
+                log = Aoutput(txt, rtup.check_sum);
+            }
+            else if (rtup.sensor_ID == 02) //전류 입력{
+            {
+                ck_sum = txt.Split('-')[0] + txt.Split('-')[1] + txt.Split('-')[2] + txt.Split('-')[3] + txt.Split('-')[4] + txt.Split('-')[5];
+                rtup.check_sum = Encoding.UTF8.GetBytes(ck_sum);
+                Ainput(txt);
+            }
+            return log;
+        }
+        
+        public string Aoutput(string txt, byte[] ck_sum)
+        {
+            string ch_name = string.Empty;
+            string log = string.Empty;
+            if (txt.Split('-')[4] == "00")
+            {
+                
+                ch_name = "00";
+            }
+            else ch_name = "01";
+
+            return ch_name;
+        }
+        public void Ainput(string txt)
+        {
+
         }
     }
 }
--- TCPSocketCl/RTUP.cs
+++ TCPSocketCl/RTUP.cs
@@ -12,14 +12,25 @@ namespace TCPSocketCl
 {
     class RTUP
     {
+        //POE 응답대기? 신호 02-73-07-0E-00-8A-03
+        //public byte sof = 0x02;
+        //public byte usys_device_ID = 0x74;
+        //public byte length = 0x09;
+        //public byte sensor_ID;
+        //public byte ch_setting;
+        //public byte data;
+        //public byte[] check_sum = new byte[2];
+        //public byte eof = 0x03;
+        //public byte response_channel;
         public byte sof = 0x02;
-        public byte usys_device_ID = 0x74;
+        public byte usys_device_ID = 0x73;
         public byte length = 0x09;
         public byte sensor_ID;
         public byte ch_setting;
         public byte data;
-        public byte check_sum;
+        public byte[] check_sum = new byte[2];
         public byte eof = 0x03;
         public byte response_channel;
+
     }
 }
